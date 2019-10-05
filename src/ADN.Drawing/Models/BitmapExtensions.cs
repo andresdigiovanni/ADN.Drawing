984b0ea3a752f6f5947be9897086cf206279be75
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2019-10-05 17:15:04 +0200

    Added methods to BitmapExtensions.

--- src/ADN.Drawing/Models/BitmapExtensions.cs
+++ src/ADN.Drawing/Models/BitmapExtensions.cs
@@ -12,9 +12,36 @@ namespace ADN.Drawing
     public static class BitmapExtensions
     {
         /// <summary>
+        /// Crop a Bitmap.
+        /// </summary>
+        /// <param name="source">Bitmap to crop.</param>
+        /// <param name="section">Section to crop the bitmap.</param>
+        /// <returns>Croped image.</returns>
+        /// <example>
+        /// <code lang="csharp">
+        /// var originalBitmap = (Bitmap)Bitmap.FromFile("polar-bear.png");
+        /// int width = 320;
+        /// int height = 180;
+        /// int x = 200;
+        /// int y = 200;
+        /// var rectangle = new Rectangle(new Point(x, y), new Size(width, height));
+        /// var cropedBitmap = originalBitmap.CropImage(rectangle);
+        /// </code>
+        /// </example>
+        public static Bitmap CropImage(this Bitmap source, Rectangle section)
+        {
+            var bitmap = new Bitmap(section.Width, section.Height);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+                return bitmap;
+            }
+        }
+
+        /// <summary>
         /// Resize a Bitmap.
         /// </summary>
-        /// <param name="bitmap">Bitmap to resize.</param>
+        /// <param name="source">Bitmap to resize.</param>
         /// <param name="width">New width.</param>
         /// <param name="height">New height.</param>
         /// <param name="pixelFormat">Specifies the format of the color data for each pixel in the image.</param>
@@ -27,14 +54,15 @@ namespace ADN.Drawing
         /// var resizedBitmap = originalBitmap.Resize(width, height);
         /// </code>
         /// </example>
-        public static Bitmap Resize(this Bitmap bitmap, int width, int height, PixelFormat pixelFormat = PixelFormat.Format32bppRgb)
+        public static Bitmap Resize(this Bitmap source, int width, int height, PixelFormat pixelFormat = PixelFormat.Format32bppRgb)
         {
-            Bitmap temp = new Bitmap(width, height, pixelFormat);
-            temp.SetResolution(bitmap.HorizontalResolution, bitmap.VerticalResolution);
-            Graphics g = Graphics.FromImage(temp);
-            g.DrawImage(bitmap, 0, 0, width, height);
-
-            return temp;
+            Bitmap bitmap = new Bitmap(width, height, pixelFormat);
+            bitmap.SetResolution(source.HorizontalResolution, source.VerticalResolution);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.DrawImage(source, 0, 0, width, height);
+                return bitmap;
+            }
         }
 
         /// <summary>
@@ -66,7 +94,7 @@ namespace ADN.Drawing
         /// <summary>
         /// Set background of a Bitmap.
         /// </summary>
-        /// <param name="bitmap">Bitmap to change background.</param>
+        /// <param name="source">Bitmap to change background.</param>
         /// <param name="color">Background color.</param>
         /// <param name="pixelFormat">Specifies the format of the color data for each pixel in the image.</param>
         /// <returns>Bitmap with the background changed.</returns>
@@ -77,15 +105,94 @@ namespace ADN.Drawing
         /// var backgroundBitmap = originalBitmap.SetBackgroundColor(backgroundColor);
         /// </code>
         /// </example>
-        public static Bitmap SetBackgroundColor(this Bitmap bitmap, Color color, PixelFormat pixelFormat = PixelFormat.Format32bppRgb)
+        public static Bitmap SetBackgroundColor(this Bitmap source, Color color, PixelFormat pixelFormat = PixelFormat.Format32bppRgb)
         {
-            Bitmap temp = new Bitmap(bitmap.Width, bitmap.Height, pixelFormat);
-            temp.SetResolution(bitmap.HorizontalResolution, bitmap.VerticalResolution);
-            Graphics g = Graphics.FromImage(temp);
-            g.Clear(color);
-            g.DrawImage(bitmap, Point.Empty);
+            Bitmap bitmap = new Bitmap(source.Width, source.Height, pixelFormat);
+            bitmap.SetResolution(source.HorizontalResolution, source.VerticalResolution);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.Clear(color);
+                g.DrawImage(source, Point.Empty);
+                return bitmap;
+            }
+        }
 
-            return temp;
+        /// <summary>
+        /// Convert a Bitmap into an array. Where the first dimension corresponds to the colors R, G and B.
+        /// </summary>
+        /// <param name="bitmap">Bitmap to transform.</param>
+        /// <returns>RGB array corresponding to the Bitmap.</returns>
+        /// <example>
+        /// <code lang="csharp">
+        /// var image = (Bitmap)Bitmap.FromFile("polar-bear.png");
+        /// var imageRGB = image.ToArrayRGB();
+        /// </code>
+        /// </example>
+        public static int[,,] ToArrayRGB(this Bitmap bitmap)
+        {
+            var arrayRGB = new int[3, bitmap.Width, bitmap.Height];
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    var pixel = bitmap.GetPixel(x, y);
+                    arrayRGB[0, x, y] = pixel.R;
+                    arrayRGB[1, x, y] = pixel.G;
+                    arrayRGB[2, x, y] = pixel.B;
+                }
+            }
+
+            return arrayRGB;
+        }
+
+        /// <summary>
+        /// Convert a grayscale image array to Bitmap.
+        /// </summary>
+        /// <param name="image">Grayscale image array.</param>
+        /// <returns>Grayscale Bitmap.</returns>
+        public static Bitmap ToBitmap(this int[,] image)
+        {
+            var bitmap = new Bitmap(image.GetLength(0), image.GetLength(1));
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var pixel = Color.FromArgb(image[i, j], image[i, j], image[i, j]);
+                    bitmap.SetPixel(i, j, pixel);
+                }
+            }
+
+            return bitmap;
+        }
+
+        /// <summary>
+        /// Convert a RGB array into a Bitmap.
+        /// </summary>
+        /// <param name="image">RGB array to transform. Where the first dimension corresponds to the color R, G and B.</param>
+        /// <returns>Transformed Bitmap.</returns>
+        /// <example>
+        /// <code lang="csharp">
+        /// var image = (Bitmap)Bitmap.FromFile("polar-bear.png");
+        /// var imageRGB = image.ToArrayRGB();
+        /// var origImage = imaRGB.ToBitmap();
+        /// </code>
+        /// </example>
+        public static Bitmap ToBitmap(this int[,,] image)
+        {
+            var bitmap = new Bitmap(image.GetLength(1), image.GetLength(2));
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var pixel = Color.FromArgb(image[0, i, j], image[1, i, j], image[2, i, j]);
+                    bitmap.SetPixel(i, j, pixel);
+                }
+            }
+
+            return bitmap;
         }
     }
 }
--- tests/ADN.Drawing.Tests/Models/BitmapExtensionsTest.cs
+++ tests/ADN.Drawing.Tests/Models/BitmapExtensionsTest.cs
@@ -1,5 +1,4 @@
-﻿using ADN.Drawing;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -14,16 +13,31 @@ namespace ADN.Drawing.Tests
     public class BitmapExtensionsTest
     {
         [Theory]
+        [InlineData("polar-bear-640-423.jpg", "polar-bear-crop.jpg", 250, 200, 250, 100)]
+        public void CropImage(string origImgFile, string expectedImgFile, int width, int height, int x, int y)
+        {
+            var origImg = (Bitmap)Bitmap.FromFile(Path.Combine("assets", origImgFile));
+            var expectedImg = (Bitmap)Bitmap.FromFile(Path.Combine("assets", expectedImgFile));
+
+            var rectangle = new Rectangle(new Point(x, y), new Size(width, height));
+            var resultImg = origImg.CropImage(rectangle);
+
+            var expectedBytesImg = ImageToBytes(expectedImg, ImageFormat.Jpeg);
+            var resultBytesImg = ImageToBytes(resultImg, ImageFormat.Jpeg);
+            Assert.Equal(expectedBytesImg, resultBytesImg);
+        }
+
+        [Theory]
         [InlineData("polar-bear-640-423.jpg", "polar-bear-320x212.jpg", 320, 212)]
         public void Resize(string origImgFile, string expectedImgFile, int width, int height)
         {
             var origImg = (Bitmap)Bitmap.FromFile(Path.Combine("assets", origImgFile));
             var expectedImg = (Bitmap)Bitmap.FromFile(Path.Combine("assets", expectedImgFile));
-            var expectedBytesImg = ImageToBytes(expectedImg, ImageFormat.Jpeg);
 
             var resultImg = origImg.Resize(width, height);
-            var resultBytesImg = ImageToBytes(resultImg, ImageFormat.Jpeg);
 
+            var expectedBytesImg = ImageToBytes(expectedImg, ImageFormat.Jpeg);
+            var resultBytesImg = ImageToBytes(resultImg, ImageFormat.Jpeg);
             Assert.Equal(expectedBytesImg, resultBytesImg);
         }
 
@@ -47,11 +61,11 @@ namespace ADN.Drawing.Tests
         {
             var origImg = (Bitmap)Bitmap.FromFile(Path.Combine("assets", origImgFile));
             var expectedImg = (Bitmap)Bitmap.FromFile(Path.Combine("assets", expectedImgFile));
-            var expectedBytesImg = ImageToBytes(expectedImg, ImageFormat.Png);
 
             var resultImg = origImg.SetBackgroundColor(Color.FromName(colorName));
-            var resultBytesImg = ImageToBytes(resultImg, ImageFormat.Png);
 
+            var expectedBytesImg = ImageToBytes(expectedImg, ImageFormat.Png);
+            var resultBytesImg = ImageToBytes(resultImg, ImageFormat.Png);
             Assert.Equal(expectedBytesImg, resultBytesImg);
         }
 
